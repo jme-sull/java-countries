9508a190d05390f80ecf21f81a1127e8946049fd
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-07-10 09:27:50 -0400

    set up controller, compiled, endpoint sucessfully returns data

--- javacountries/src/main/java/local/jmesull/javacountries/repositories/CountryRepository.java
+++ javacountries/src/main/java/local/jmesull/javacountries/repositories/CountryRepository.java
@@ -3,6 +3,6 @@ package local.jmesull.javacountries.repositories;
 import local.jmesull.javacountries.models.Country;
 import org.springframework.data.repository.CrudRepository;
 
-public interface EmployeeRepository extends CrudRepository<Country, Long>
+public interface CountryRepository extends CrudRepository<Country, Long>
 {
 }
